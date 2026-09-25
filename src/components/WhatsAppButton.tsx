9034ec199f66552505f5ef56c81779c5b9227e5a
craftlanee-tech <craftlanee@gmail.com>
2026-09-25 15:30:39 +0530

    Added all

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -55,7 +55,7 @@ export default function WhatsAppButton() {
 
   return (
     <motion.div
-      className="pointer-events-none fixed bottom-4 right-2 top-20 z-[70] flex flex-col items-center sm:right-4"
+      className="pointer-events-none fixed bottom-4 right-2 top-20 z-[70] hidden flex-col items-center sm:right-4 sm:flex"
       animate={{ opacity: visible ? 1 : 0 }}
       transition={{ duration: 0.3 }}
     >
